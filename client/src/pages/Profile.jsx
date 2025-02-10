0540a394addab8f750d5ae83fc8793d6929b938b
Shubham Mukherjee <shubham.aot@gmail.com>
2025-02-10 14:55:21 +0530

    completed image upload without firebase storage

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,72 @@
 import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+// import {
+//   getDownloadURL,
+//   getStorage,
+//   ref,
+//   uploadBytesResumable,
+// } from "firebase/storage";
+// import { app } from "../firebase";
 
 const Profile = () => {
+  const fileRef = useRef();
+  const [image, setImage] = useState(undefined);
   const { currentUser } = useSelector((state) => state.user);
+  // const [imagePercent, setImagePercent] = useState(0);
+  const [formData, setFormData] = useState({});
+
+  useEffect(() => {
+    if (image) {
+      // handleFileUpload(image);
+      setFormData({ ...formData, profilePicture: URL.createObjectURL(image) });
+    }
+  }, [image]);
+
+  // const handleFileUpload = async (image) => {
+  //   const storage = getStorage(app);
+  //   const fileName = new Date().getTime() + "-" + image.name;
+  //   const storageRef = ref(storage, fileName);
+  //   const uploadTask = uploadBytesResumable(storageRef, image);
+  //   uploadTask.on(
+  //     "state_changed",
+  //     (snapshot) => {
+  //       const progress =
+  //         (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+  //       setImagePercent(Math.round(progress));
+  //     },
+  //     (error) => {
+  //       console.log(error);
+  //     },
+  //     () => {
+  //       getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+  //         setFormData({ ...formData, profilePicture: downloadURL })
+  //       );
+  //     }
+  //   );
+  // };
+
+  console.log(formData, "formData");
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className="flex flex-col gap-4">
+        <input
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+          onChange={(e) => setImage(e.target.files[0])}
+        />
         <img
-          src={currentUser.profilePicture}
+          src={
+            formData.profilePicture
+              ? formData.profilePicture
+              : currentUser.profilePicture
+          }
           alt="profile"
           className="h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2"
+          onClick={() => fileRef.current.click()}
         />
         <input
           defaultValue={currentUser.username}
